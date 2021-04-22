aa91108397d65644d6ce7a7aae4d13f869cee9de
repinamaria <mari.repina.2002@mail.ru>
2021-04-23 00:44:48 +0300

    Update

--- src/main/java/TailLauncher.java
+++ src/main/java/TailLauncher.java
@@ -49,7 +49,7 @@ public class TailLauncher {
 
 
     public static void main(String[] args) {
-        List<String> res = new TailLauncher().launch("-o","output/output.txt", "input/file1.txt", "input/file0.txt");
+        List<String> res = new TailLauncher().launch(args);
         System.out.println(res);
     }
 
